04bf15a86bf016ae6bc0fc7cc658ed2e4c2ae50e
ieeah <lbnfnc90@gmail.com>
2021-11-11 14:50:09 +0100

    git end

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        heading: 'Hello World!',
+        imgSrc: 'https://images.unsplash.com/photo-1611618925739-86ac255ac4ce?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60',
+        imgClass: 'img',
+    },
+    methods: {
+
+    },
+});
